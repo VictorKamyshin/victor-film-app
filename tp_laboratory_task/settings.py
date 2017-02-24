7117847a8b2cf5668623d3e1a3ec7473f382b559
Victor <Kamyshin.Victor@yandex.ru>
2017-02-24 18:12:40 +0300

    problem with static

--- tp_laboratory_task/settings.py
+++ tp_laboratory_task/settings.py
@@ -149,9 +149,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
