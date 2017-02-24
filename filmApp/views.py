a6dd53934dec8663efaa175873191bd9bdf0ae8c
Victor <Kamyshin.Victor@yandex.ru>
2017-02-24 18:50:06 +0300

    comment order

--- filmApp/views.py
+++ filmApp/views.py
@@ -443,19 +443,19 @@ def film_comment(request):
             comment = Comment.customManager.create_comment(text=text, author=author, parent=parent_comment, film=film)
             print film.count_of_comments
             previous_comments = list(Comment.customManager.filter(material_path__lt=comment.material_path,
-                                                                  film__id=film.id))
+                                                                  film__id=film.id).order_by('material_path'))
             if len(previous_comments) > 0:
                 previous_comment = previous_comments[-1]
                 return HttpResponse(json.dumps({'text': 'Hello!', 'level': comment.level,
-                                            'reverse_level': 12 - comment.level, 'comment_id': comment.id,
-                                            'username': request.user.username, 'prev_comment_id': previous_comment.id}),
-                                content_type='application/json')
+                                                'reverse_level': 12 - comment.level, 'comment_id': comment.id,
+                                                'username': request.user.username,
+                                                'prev_comment_id': previous_comment.id}),
+                                    content_type='application/json')
             else:
                 return HttpResponse(json.dumps({'text': 'Hello!', 'level': comment.level,
-                                            'reverse_level': 12 - comment.level, 'comment_id': comment.id,
-                                            'username': request.user.username, 'prev_comment_id': None}),
-                                content_type='application/json')
-
+                                                'reverse_level': 12 - comment.level, 'comment_id': comment.id,
+                                                'username': request.user.username, 'prev_comment_id': None}),
+                                    content_type='application/json')
         else:
             return HttpResponseBadRequest('Unsupported request method')
 
